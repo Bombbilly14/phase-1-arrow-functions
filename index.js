e3e095bf52d77328edcdbef06536b44b29bd5745
William German <william.j.german2@gmail.com>
2022-11-05 16:50:33 -0700

    commit tree 539964218fe1b7d1cbe744ae60ab97f25b5c9e3e

---
+++ index.js
@@ -0,0 +1,34 @@
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+const square = (num1) => num1 *num1
+
+const add = (num1, num2) => num1 + num2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function divide (){
+//     return 2000/100
+// }
+// const square = (num1) => {
+//     return num1 * num1
+// }
+// const add = (num2, num3) => {
+//     return num2+num3
+// }
